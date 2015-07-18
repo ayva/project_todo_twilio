7ab536df1d7fe90abb7d73cf0d5114952b4f41d1
Garrett <garrettweinert@gmail.com>
2015-07-18 14:53:47 -0700

    db:migrate redo

--- todo_twillio/app/models/todo.rb
+++ todo_twillio/app/models/todo.rb
@@ -1,2 +1,23 @@
-class Todo < ActiveRecord::Base
+class Todo
+
+  # class << self; attr_accessor :instances; end
+  
+  # attr_accessor :id, :text, :date
+  
+  # def initialize(id=nil, text=nil, date=nil)
+
+  #   @id = id
+  #   @text = text
+  #   @date = date
+
+  #   self.class.instances ||= Array.new
+  #   self.class.instances << self
+
+
+  # end
+
+  # def self.all
+  #   instances ? instances.dup : []
+  # end
+
 end
