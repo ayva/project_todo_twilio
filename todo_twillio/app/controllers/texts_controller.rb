e798fe4deed06567edb952f5c78d55f2921b92b4
ayva <olga.ayva@gmail.com>
2015-07-18 16:51:33 -0700

    making twilio work

--- todo_twillio/app/controllers/texts_controller.rb
+++ todo_twillio/app/controllers/texts_controller.rb
@@ -3,8 +3,9 @@ class TextsController < ApplicationController
   
 
   def send_text
-    account_sid = File.readlines("key.md")[0]
-    auth_token =  File.readlines("key.md")[1]
+    
+    account_sid = File.readlines("key.md")[0].strip
+    auth_token =  File.readlines("key.md")[1].strip
 
     # set up a client to talk to the Twilio REST API
     @text = Twilio::REST::Client.new account_sid, auth_token
@@ -13,7 +14,8 @@ class TextsController < ApplicationController
     @text.messages.create(
       from: File.readlines("key.md")[3].strip,
       to: File.readlines("key.md")[2].strip,
-      body: 'Hey there!'
+      body: "#{@todo.text}"
     )
+    flash.notice = "SMS '#{@todo.text}' was sent!"
   end
 end
